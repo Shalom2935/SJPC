2ce4a87e119902d018536a3c9e17962aa0659b14
Shalom <kamokoueshalom@gmail.com>
2023-10-20 08:57:04 +0200

    folders organization

---
+++ src/components/Buttons/buttonSmall.js
@@ -0,0 +1,10 @@
+import './Buttons.scss';
+function ButtonSmall(){
+    return(
+        <div className="button">
+            <button>REJOINDRE</button>
+        </div>
+    );
+}
+
+export default ButtonSmall;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import './Header.scss';
+import '../Buttons/buttonSmall';
+import ButtonSmall from '../Buttons/buttonSmall';
+
+function Header() {
+  return(
+    <div className='header'>
+        <div className='logo'>Logo</div>
+        <div className='navbar'>
+            <ul className='navbar-items'>
+                <li>ACCUEIL</li>
+                <li>CONTACT</li>
+                <li><ButtonSmall /></li>
+            </ul>
+            
+        </div>
+    </div>
+  );
+}
+
+export default Header;
