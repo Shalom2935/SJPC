58b40d27edb91d2775073cf9ca5e0ab3828968c3
RoyalsLab <115007029+RoyalsLab@users.noreply.github.com>
2023-12-13 09:03:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../src/Pages/Home/Home';
 import Contact from '../src/Pages/Contact/Contact';
 import Inscription from '../src/Pages/Inscription/Inscription';
-let express = require('express');
-let exp = express();
-
-exp.post("/name", function (req, res) {
-    const { first, last } = req.body;
-    res.json({
-        name: `${first} ${last}`
-    });
-});
 
 function App() {
     return (
